b7057dfcfa916d5fd093130278238d861fe0925a
Ved Gupta <vedgupta@protonmail.com>
2022-04-30 19:51:31 +0530

    test

--- razorOrderGenrate.js
+++ razorOrderGenrate.js
@@ -54,7 +54,7 @@ app.post('/createorder', async (req, res) => {
 
 	try {
 		const response = await razorpay.orders.create(options);
-		console.log(response);
+		// console.log(response);
 		res.json({
 			id: response.id,
 			currency: response.currency,
