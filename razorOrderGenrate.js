85d3815a36b73e46577674ca2b3075a947a913dc
Sudhanshu <rishuparashar7@gmail.com>
2022-04-30 18:39:11 +0530

    written on sudhanshu laptop 30-04

--- razorOrderGenrate.js
+++ razorOrderGenrate.js
@@ -33,7 +33,7 @@ app.post('/createorder', async (req, res) => {
 
 	try {
 		const response = await razorpay.orders.create(options);
-		console.log(response);
+		// console.log(response);
 		res.json({
 			id: response.id,
 			currency: response.currency,
