80cc3e908c065b7bcafe06bef5a51cf5be5877a3
Ved Gupta <vedgupta@protonmail.com>
2022-04-30 19:41:37 +0530

    verification endpoint developed not tested

--- razorOrderGenrate.js
+++ razorOrderGenrate.js
@@ -3,9 +3,12 @@ const Razorpay = require('razorpay');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+const crypto = require('crypto');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+require('dotenv').config()
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST || 'localhost';
@@ -15,6 +18,25 @@ const KEYSECRET = process.env.KEYSECRET;
 const razorpay = new Razorpay({
 	key_id: KEYID,
 	key_secret: KEYSECRET
+})
+
+app.post('/verification', (req, res) => {
+	// req.body = {"order_id": razorpayOrderId, "payment_id": razorpayPaymentId }
+	// header : req.headers['x-razorpay-signature']
+	// console.log(req.body);
+
+	const secret = '12345678'
+	const shasum = crypto.createHmac('sha256', secret);
+	shasum.update(JSON.stringify(req.body));
+
+	const digest = shasum.digest('hex');
+	console.log(digest, req.headers['x-razorpay-signature'])
+
+	if (digest === req.headers['x-razorpay-signature']) {
+		res.json({ status: true })
+	} else {
+		res.json({ status: false })
+	}
 })
 
 
@@ -32,7 +54,7 @@ app.post('/createorder', async (req, res) => {
 
 	try {
 		const response = await razorpay.orders.create(options);
-		// console.log(response);
+		console.log(response);
 		res.json({
 			id: response.id,
 			currency: response.currency,
