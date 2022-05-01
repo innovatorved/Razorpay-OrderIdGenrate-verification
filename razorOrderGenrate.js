94fc5f999a8be68e436515ea1a8a9e7e717d718b
Ved Gupta <vedgupta@protonmail.com>
2022-05-01 14:02:51 +0530

    testing complte verification endpoint working

--- razorOrderGenrate.js
+++ razorOrderGenrate.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const crypto = require('crypto');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 require('dotenv').config()
 
@@ -24,13 +24,15 @@ app.post('/verification', (req, res) => {
 	// req.body = {"order_id": razorpayOrderId, "payment_id": razorpayPaymentId }
 	// header : req.headers['x-razorpay-signature']
 	// console.log(req.body);
+	// console.log(req.body.payload);
 
 	const secret = process.env.WEBHOOK_SECRET;
+
 	const shasum = crypto.createHmac('sha256', secret);
 	shasum.update(JSON.stringify(req.body));
-
 	const digest = shasum.digest('hex');
-	console.log(digest, req.headers['x-razorpay-signature'])
+
+	// console.log(digest, req.headers['x-razorpay-signature'])
 
 	if (digest === req.headers['x-razorpay-signature']) {
 		res.json({ status: true })
@@ -38,11 +40,16 @@ app.post('/verification', (req, res) => {
 		// process whatever zyou like
 	} else {
 		res.json({ status: false })
+		console.log("not verified")
 	}
 })
 
 
 app.post('/createorder', async (req, res) => {
+	if(!req.body.amount){
+		return res.status(503).json({status:false , error:"Enter the Amount"});
+
+	}
 	const payment_capture = 1;
 	const amount = req.body.amount;
 	const currency = req.body.currency || 'INR';
@@ -56,14 +63,14 @@ app.post('/createorder', async (req, res) => {
 
 	try {
 		const response = await razorpay.orders.create(options);
-		// console.log(response);
 		res.json({
+			status:true,
 			id: response.id,
 			currency: response.currency,
 			amount: response.amount
 		})
 	} catch (error) {
-		console.log(error)
+		return res.status(503).json({status:false , error:error.message});
 	}
 })
 
